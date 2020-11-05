3b54bf929d9174e08bca19590fa71e864aec4bbb
Jurtsal <joshburtsal@gmail.com>
2020-11-05 16:50:32 +0000

    Added FAQs

--- app/data/beach-data.tsx
+++ app/data/beach-data.tsx
@@ -32,8 +32,8 @@ const BeachData = [
 		cycling: cycling[1],
 		bbqs: bbqs[2],
 		congestion: congestion[2],
-    },
-    {
+	},
+	{
 		id: 3,
 		name: "Alum Chine",
 		lifeguarded: lifeguarded[0],
@@ -43,7 +43,7 @@ const BeachData = [
 		bbqs: bbqs[2],
 		congestion: congestion[0],
 	},
-    {
+	{
 		id: 4,
 		name: "Bournemouth West",
 		lifeguarded: lifeguarded[1],
@@ -53,7 +53,7 @@ const BeachData = [
 		bbqs: bbqs[2],
 		congestion: congestion[1],
 	},
-    {
+	{
 		id: 5,
 		name: "Bournemouth Pier",
 		lifeguarded: lifeguarded[1],
@@ -63,7 +63,7 @@ const BeachData = [
 		bbqs: bbqs[1],
 		congestion: congestion[2],
 	},
-    {
+	{
 		id: 6,
 		name: "Bournemouth East",
 		lifeguarded: lifeguarded[1],
@@ -73,7 +73,7 @@ const BeachData = [
 		bbqs: bbqs[1],
 		congestion: congestion[0],
 	},
-    {
+	{
 		id: 7,
 		name: "Boscombe West",
 		lifeguarded: lifeguarded[0],
@@ -83,7 +83,7 @@ const BeachData = [
 		bbqs: bbqs[1],
 		congestion: congestion[1],
 	},
-    {
+	{
 		id: 8,
 		name: "Boscombe East",
 		lifeguarded: lifeguarded[0],
@@ -93,7 +93,7 @@ const BeachData = [
 		bbqs: bbqs[2],
 		congestion: congestion[0],
 	},
-    {
+	{
 		id: 9,
 		name: "Southborne West",
 		lifeguarded: lifeguarded[0],
@@ -103,7 +103,7 @@ const BeachData = [
 		bbqs: bbqs[2],
 		congestion: congestion[0],
 	},
-    {
+	{
 		id: 10,
 		name: "Southborne East",
 		lifeguarded: lifeguarded[0],
@@ -113,7 +113,7 @@ const BeachData = [
 		bbqs: bbqs[2],
 		congestion: congestion[1],
 	},
-    {
+	{
 		id: 11,
 		name: "Hengistbury Head",
 		lifeguarded: lifeguarded[0],
@@ -123,7 +123,7 @@ const BeachData = [
 		bbqs: bbqs[2],
 		congestion: congestion[2],
 	},
-    {
+	{
 		id: 12,
 		name: "Mudeford",
 		lifeguarded: lifeguarded[0],
@@ -132,7 +132,7 @@ const BeachData = [
 		cycling: cycling[1],
 		bbqs: bbqs[2],
 		congestion: congestion[0],
-	}
+	},
 ];
 
 export default BeachData;
--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import { Button, Card } from "react-native-elements";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons"; // https://github.com/oblador/react-native-vector-icons
+import SiteColours from "../assets/colours/site-colours";
 
 import StarredCard from "../components/cards/starred-card";
 import Map from "./map";
@@ -11,15 +12,24 @@ const Home = ({ navigation }: any) => {
 		<View style={{ flex: 1, alignItems: "center" }}>
 			<StarredCard></StarredCard>
 			<Button
+				containerStyle={{ width: "100%" }}
 				title="View all beaches"
+				titleStyle={{ color: SiteColours.primary }}
 				type="clear"
-				icon={<MaterialIcon name="search" size={26} />}
+				icon={<MaterialIcon name="search" size={26} color={SiteColours.primary} />}
 				onPress={() => navigation.jumpTo("AllBeaches")}></Button>
 
 			<Card containerStyle={{ height: 230, width: "100%" }}>
 				<Map preview={true}></Map>
 			</Card>
-			<Button title="View full map" type="clear" icon={<MaterialIcon name="map" size={26} />} onPress={() => navigation.jumpTo("Map")}></Button>
+
+			<Button
+				containerStyle={{ width: "100%" }}
+				title="View full map"
+				titleStyle={{ color: SiteColours.primary }}
+				type="clear"
+				icon={<MaterialIcon name="map" size={26} color={SiteColours.primary} />}
+				onPress={() => navigation.jumpTo("Map")}></Button>
 		</View>
 	);
 };
